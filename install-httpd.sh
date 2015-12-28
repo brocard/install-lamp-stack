efd9d5f616f3697ecccf9346cf7168d0c75f4190
Yusniel Brocard <brocard@gmail.com>
2015-12-28 14:01:10 -0600

    Update install-httpd.sh

--- install-httpd.sh
+++ install-httpd.sh
@@ -5,8 +5,14 @@ if [ `id -u` -ne '0' ]; then
   exit 1
 fi
 
+wget http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+
 #install httpd for Centos 6.x
 sudo yum -y install httpd httpd-devel php php-mysql php-common php-gd php-mbstring php-mcrypt php-devel php-xml
+
+sudo yum update
 
 #only firt time install
 sudo service httpd start
