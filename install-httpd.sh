90721acc8f77150a98a30c18270508d849f75d42
Yusniel Brocard <brocard@gmail.com>
2015-12-28 12:36:38 -0600

    Update install-httpd.sh

--- install-httpd.sh
+++ install-httpd.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ `id -u` -ne '0' ]; then
+  echo "This script must be run as root" >&2
+  exit 1
+fi
+
 #install httpd for Centos 6.x
 sudo yum -y install httpd httpd-devel php php-mysql php-common php-gd php-mbstring php-mcrypt php-devel php-xml
