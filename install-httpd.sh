9e390edabfb7871671bfb06266b1d44b7daf2c95
Yusniel Brocard <brocard@gmail.com>
2015-12-28 17:07:45 -0600

    Update install-httpd.sh

--- install-httpd.sh
+++ install-httpd.sh
@@ -13,7 +13,7 @@ wget http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 
-echo -e "\e[31mEnabled repo php56\e[0m\n"
+echo -e "\e[31m\e[1mEnabled repo php56\e[0m\n"
 sed -i '/\[remi-php56\]/,/enabled=0/ { s/enabled=0/enabled=1/ }' /etc/yum.repos.d/remi.repo
 
 #install httpd for Centos 6.x
@@ -22,7 +22,7 @@ sudo yum -y install httpd httpd-devel php php-mysql php-common php-gd php-mbstri
 sudo yum update -y
 
 #Only first time to install
-echo "star httpd service"
+echo -e "\e[31m\e[1mStar httpd service\e[0m"
 sudo service httpd start
 
 #to enable service
@@ -33,7 +33,7 @@ echo "Installing composer\n"
 if [ ! -x /usr/bin/wget ] ; then
     curl -s https://getcomposer.org/installer | php
     mv composer.phar /usr/local/bin/composer
-    echo "Done!"
+    echo -e "\e[32mDone!\e[0m"
 else
     echo "Composer is installed"
 fi
