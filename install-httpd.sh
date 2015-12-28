26c5e192ef06c4395975be6fde102df8b2db837f
Yusniel Brocard <brocard@gmail.com>
2015-12-28 13:25:24 -0600

    Update install-httpd.sh

--- install-httpd.sh
+++ install-httpd.sh
@@ -7,3 +7,6 @@ fi
 
 #install httpd for Centos 6.x
 sudo yum -y install httpd httpd-devel php php-mysql php-common php-gd php-mbstring php-mcrypt php-devel php-xml
+
+#to enable service
+sudo chkconfig httpd on
