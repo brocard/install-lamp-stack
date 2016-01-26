78980e74eff261a89462719dfc12b8085dc83af4
Yusniel Brocard <brocard@gmail.com>
2016-01-26 09:16:11 -0600

    Update install-httpd.sh

--- install-httpd.sh
+++ install-httpd.sh
@@ -9,7 +9,7 @@ if [ ! -x /usr/bin/wget ] ; then
     yum install wget -y
 fi
 
-wget http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 
